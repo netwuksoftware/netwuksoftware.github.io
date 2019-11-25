18c26615775149d191686be707f129ec95475f04
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2019-11-25 23:59:33 +0530

    paddle links and tags feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,13 +50,13 @@ app.controller('notFoundCtrl', function($scope, $location) {
 });
 
 const items = [
-	{ name: 'DevPad (JAR + Source Code)', desc: 'DevPad made in Java (with Source code)', amt: '$5.00', pay: '214', id: 'devpad-jar-source', hi: true },
-	{ name: 'Check Mate Chess Game', desc: 'Check Mate Chess Game made in Java (with Source code)', amt: '$8.00', pay: '', id: 'checkmate-jar-source', hi: true },
-	{ name: 'Check Mate Chess Game (Multiplayer)', desc: 'Check Mate Chess Game (Multiplayer) made in Java (with Source code)', amt: '$10.00', pay: '', id: 'checkmate-multi-jar-source', hi: true },
-	{ name: 'Super Mario Bros Game Clone', desc: 'Super Mario Bros Clone made in Java (with Source code)', amt: '$15.00', pay: '', id: 'super-mario-bros-jar-source', hi: true },
-	{ name: 'Super Mario Bros Clone + Check Mate (with Source Code)', desc: 'Super Mario Bros Clone and Check Mate games made in Java (with Source code)', amt: '$25.00', pay: '', id: 'games-combo-jar-source', hi: true },
-	{ name: 'MIN Browser', desc: '[Only for Windows] MIN Browser', amt: '$18.00', pay: '', id: 'min-browser-windows-exe', hi: true },
-	{ name: 'MIN Browser (with Advanced Dev Tools)', desc: '[Only for Windows] MIN Browser', amt: '$35.00', pay: '', id: 'min-browser-advanced-exe', hi: true },
-	{ name: 'CaptureX Surveillance', desc: '[Only for Windows] CaptureX Surveillence Software Setup', amt: '$30.00', pay: '', id: 'capturex-windows-exe', hi: false },
-	{ name: 'CaptureX Surveillance (with Sharing Feature)', desc: '[Only for Windows] CaptureX Surveillance (with Sharing Feature)', amt: '$40.00', pay: '', id: 'capturex-advanced-exe', hi: false }
+	{ name: 'DevPad (JAR + Source Code)', desc: 'DevPad made in Java (with Source code)', amt: '$5.00', pay: '576799', id: 'devpad-jar-source', hi: true, tech: ['Java', 'All Platforms'] },
+	{ name: 'Check Mate Chess Game', desc: 'Check Mate Chess Game made in Java (with Source code)', amt: '$8.00', pay: '576800', id: 'checkmate-jar-source', hi: true, tech: ['Java', 'Libgdx', 'All Platforms'] },
+	{ name: 'Check Mate Chess Game (Multiplayer using SocketIO)', desc: 'Check Mate Chess Game (Multiplayer) made in Java (with Source code)', amt: '$10.00', pay: '576802', id: 'checkmate-multi-jar-source', hi: true, tech: ['Java', 'Libgdx', 'SocketIO', 'All Platforms'] },
+	{ name: 'Super Mario Bros Game Clone', desc: 'Super Mario Bros Clone made in Java (with Source code)', amt: '$15.00', pay: '576804', id: 'super-mario-bros-jar-source', hi: true, tech: ['Java', 'Libgdx', 'All Platforms'] },
+	{ name: 'Super Mario Bros Clone + Check Mate (with Source Code)', desc: 'Super Mario Bros Clone and Check Mate games made in Java (with Source code)', amt: '$25.00', pay: '576803', id: 'games-combo-jar-source', hi: true, tech: ['Java', 'Libgdx', 'All Platforms'] },
+	{ name: 'MIN Browser', desc: '[Only for Windows] MIN Browser', amt: '$18.00', pay: '576805', id: 'min-browser-windows-exe', hi: true, tech: ['Utility', 'Windows'] },
+	{ name: 'MIN Browser (with Advanced Dev Tools)', desc: '[Only for Windows] MIN Browser (with Advanced Dev Tools to help designers and developers)', amt: '$35.00', pay: '576806', id: 'min-browser-advanced-exe', hi: true, tech: ['Utility', 'Dev Tools', 'Windows'] },
+	{ name: 'CaptureX Surveillance', desc: '[Only for Windows] CaptureX Surveillence Software Setup', amt: '$30.00', pay: '576806', id: 'capturex-windows-exe', hi: false, tech: ['Utility', 'Windows'] },
+	{ name: 'CaptureX Surveillance (with Sharing Feature)', desc: '[Only for Windows] CaptureX Surveillance (with Sharing Feature)', amt: '$40.00', pay: '576807', id: 'capturex-advanced-exe', hi: false, tech: ['Utility', 'Windows'] }
 ];
